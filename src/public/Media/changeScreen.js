65c137c0843e88d942be9849e7d589b3ea183882
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-05-19 19:04:23 +0700

    Create changeScreen.js

---
+++ src/public/Media/changeScreen.js
@@ -0,0 +1,14 @@
+const showMenu = (idToggle,Idnav) =>{
+    const toggle = document.getElementById(idToggle),
+          nav = document.getElementById(Idnav)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+
+showMenu('nav-toggle','nav-menu');
