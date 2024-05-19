10ef06967d47e909d52b3c873be25e57848f7bc1
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-05-19 19:06:00 +0700

    Create handleScrollBar.js

---
+++ src/public/Media/handleScrollBar.js
@@ -0,0 +1,12 @@
+let game_bx_1 = document.getElementById('game_bx_1');
+
+let game_bx_1_left_bt = document.getElementById('game_bx_1_left_bt');
+let game_bx_1_right_bt = document.getElementById('game_bx_1_right_bt');
+
+game_bx_1_left_bt.addEventListener('click', ()=>{
+    game_bx_1.scrollLeft = game_bx_1.scrollLeft - 250;
+});
+
+game_bx_1_right_bt.addEventListener('click', ()=>{
+    game_bx_1.scrollLeft = game_bx_1.scrollLeft + 250;
+});
