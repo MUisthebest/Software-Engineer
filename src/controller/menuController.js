62459c55edbe8209184de47ebca79de1b08531bb
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-05-20 14:28:51 +0700

    I love Vinh Vui Ve

--- src/controller/menuController.js
+++ src/controller/menuController.js
@@ -6,7 +6,25 @@ const getAboutWeb = (req, res) =>{
     res.render("AboutWebsitepage.ejs");
 }
 
+const getShopFreePage = (req, res) =>{
+    res.render("Freepage.ejs");
+}
+
+const getShopBuyPage = (req, res) =>{
+    res.render("Buypage.ejs");
+}
+
+const getLovePage = (req, res) =>{
+    res.render("Lovepage.ejs");
+}
+
+const getTrendingPage = (req, res) =>{
+    res.render("Trendingpage.ejs");
+}
+
+
+
 
 module.exports = {
-    getHomePage, getAboutWeb
+    getHomePage, getAboutWeb, getShopFreePage, getShopBuyPage, getLovePage, getTrendingPage
 }
--- src/router/web.js
+++ src/router/web.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getHomePage, getAboutWeb} = require("../controller/menuController");
+const { getHomePage, getAboutWeb, getShopFreePage, getShopBuyPage, getLovePage, getTrendingPage } = require("../controller/menuController");
 
 router.get('/', getHomePage);
 
-router.get('/AboutWebsite', getHomePage);
+router.get('/AboutWebsite', getAboutWeb);
+
+router.get('/Trending', getTrendingPage);
+
+router.get('/Love', getLovePage);
+
+router.get('/Free', getShopFreePage);
+
+router.get('/Buy', getShopBuyPage);
+
+
 
 
 module.exports = router;
