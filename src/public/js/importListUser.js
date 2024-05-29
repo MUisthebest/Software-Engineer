a5140c8a304e65691d856641e072aa1d52deffe6
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-05-29 12:15:37 +0700

    import data user

--- src/public/js/importListUser.js
+++ src/public/js/importListUser.js
@@ -1,23 +1,3 @@
-const showMenu = (idToggle,Idnav) =>{
-    const toggle = document.getElementById(idToggle),
-          nav = document.getElementById(Idnav)
- 
-    toggle.addEventListener('click', () =>{
-        // Add show-menu class to nav menu
-        nav.classList.toggle('show-menu')
- 
-        // Add show-icon to show and hide the menu icon
-        toggle.classList.toggle('show-icon')
-    })
- }
-
-showMenu('nav-toggle','nav-menu');
-
-function change(){
-
-    document.querySelector("#re").innerHTML = "Profile";
-}
-
 const joinData = [
     {
         img: "/Media/background.jpg",
@@ -33,25 +13,25 @@ const joinData = [
     },
     {
         img: "/Media/background.jpg",
-        name: "Five night at feddy",
+        name: "Oggy",
         describe: "haha",
         cost: "100000"
     },
     {
         img: "/Media/background.jpg",
-        name: "Five night at feddy",
+        name: "Xì trum",
         describe: "haha",
         cost: "100000"
     },
     {
         img: "/Media/background.jpg",
-        name: "Five night at feddy",
+        name: "Hunter",
         describe: "haha",
         cost: "100000"
     },
     {
         img: "/Media/background.jpg",
-        name: "Five night at feddy",
+        name: "Meo Meo",
         describe: "haha",
         cost: "100000"
     }
