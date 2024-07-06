66bd70d4cd478b2c6e0162a7f74d82f02894c77b
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-07-07 01:47:53 +0700

    update wishlist

---
+++ src/public/js/importWishlist.js
@@ -0,0 +1,5 @@
+import { wishList } from "./wishlist";
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert(wishList.pop(0))
+})
--- src/public/js/wishlist.js
+++ src/public/js/wishlist.js
@@ -11,8 +11,6 @@ function setup()
     products.onclick = function(e) {
         localStorage.setItem('select'+productId, productId);
         updateSelectedButton(productId);
-        // addItem (e)
-        // products[i].classList.add('heart')
     }
 
     function updateSelectedButton(selected) {
@@ -27,33 +25,3 @@ function setup()
     }
 
 window.addEventListener("DOMContentLoaded", setup);
-
-// function addItem (e) {
-//     let productId = e.target.getAttribute("id");
-//     if(!wishList.find(element => element === productId)){
-//         let productDiv = document.getElementById("product" + productId);
-
-//         let wishDiv = document.createElement("div");
-//         wishDiv.setAttribute("id", "wish" + productId);
-//         wishDiv.setAttribute("class", "product");
-//         wishDiv.setAttribute("style", "margin-bottom: 10px;")
-//         // wishDiv.innerHTML += productDiv.innerHTML;
-//         let removeBtn = document.createElement("input");
-//         removeBtn.setAttribute("id", "remove" + productId);
-//         removeBtn.setAttribute("type", "button");
-//         removeBtn.setAttribute("value", "Remove");
-//         // removeBtn.setAttribute("class", "removebut");
-//         removeBtn.onclick = () => removeItem(productId);
-//         wishDiv.appendChild(removeBtn);
-
-//         let aside = document.getElementById("wishlist");
-//         // aside.appendChild(wishDiv);
-
-//         wishList.push(productId);
-//     }
-// }
-
-// function removeItem(productId) {
-//     document.getElementById("wish" + productId).remove();
-//     wishList = wishList.filter(element => element !== productId)
-// }
