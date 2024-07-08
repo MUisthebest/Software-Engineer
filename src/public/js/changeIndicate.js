70592c66725cc3f2a17dc1cca612934af83bc981
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-07-08 14:49:47 +0700

    update indicate

--- src/public/js/changeIndicate.js
+++ src/public/js/changeIndicate.js
@@ -2,61 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const buy = document.querySelector('.choose1');
     const cart = document.querySelector('.choose2');
     const login = document.querySelector('.choose3');
-    const c1 = document.querySelector('.c1');
-    const c2 = document.querySelector('.c2');
-    const c3 = document.querySelector('.c3');
-    const c4 = document.querySelector('.c4');
+    const currentUrl = window.location.href;
 
-    const selectedButton = sessionStorage.getItem('selectedButton');
-
-    updateSelectedButton(selectedButton);
-
-    buy.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'buy');
-        updateSelectedButton('buy');
-    };
-
-    cart.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'cart');
-        updateSelectedButton('cart');
-    };
-
-    login.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'login');
-        updateSelectedButton('login');
-    };
-
-    c1.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'c');
-        updateSelectedButton('c');
-    };
-
-    c2.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'c');
-        updateSelectedButton('c');
-    };
-
-    c3.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'c');
-        updateSelectedButton('c');
-    };
-
-    c4.onclick = () => {
-        sessionStorage.setItem('selectedButton', 'c');
-        updateSelectedButton('c');
-    };
+    updateSelectedButton(currentUrl);
 
 
     function updateSelectedButton(selected) {
         buy.classList.remove('select');
         cart.classList.remove('select');
         login.classList.remove('select');
-        if (selected === 'buy') {
+        if (currentUrl.includes("Buy")) {
             buy.classList.add('select');
-        } else if (selected === 'cart') {
+        } else if (currentUrl.includes("Cart")) {
             cart.classList.add('select');
-        } else if (selected === 'login') {
+        } else if (currentUrl.includes("Login")) {
             login.classList.add('select');
+        } else {
+            
         }
     }
+
 });
+
+
