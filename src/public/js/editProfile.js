3888e6d005d567ba18c480c2f0a1c668cc69b887
qtqvinh <qtqvinh22@clc.fitus.edu.vn>
2024-07-11 23:55:47 +0700

    Frontend: update user profile

---
+++ src/public/js/editProfile.js
@@ -0,0 +1,24 @@
+const changePasswordFormDOM = document.querySelector('.edit-form')
+const curPasswordField = document.getElementById('curPassword')
+const newPasswordField = document.getElementById('newPassword')
+const confirmPasswordField = document.getElementById('confirmPassword');
+const formAlertDOM = document.querySelector('.form-alert')
+const params = window.location.search
+const id = new URLSearchParams(params).get('id')
+
+changePasswordFormDOM.addEventListener('submit', async(e)=>{
+    e.preventDefault();
+    const currentPassword = curPasswordField.value;
+    const newPassword = newPasswordField.value;
+    const confirmPassword = confirmPasswordField.value;
+
+    try {
+        const {data} = await axios.patch(`/user/changePassword`, {currentPassword, newPassword, confirmPassword})
+        curPasswordField.value = '';
+        newPasswordField.value = '';
+        confirmPasswordField.value = '';
+        formAlertDOM.textContent = data.msg
+    } catch (error) {
+        formAlertDOM.textContent = error.response.data.msg
+    }
+})
