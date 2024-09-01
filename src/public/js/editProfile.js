4e24f7c047bfa582a9d8f6ecca1fbce5adc4b609
qtqvinh <qtqvinh22@clc.fitus.edu.vn>
2024-09-01 12:47:32 +0700

    Frontend: modify change password and update profile

--- src/public/js/editProfile.js
+++ src/public/js/editProfile.js
@@ -1,10 +1,41 @@
-const changePasswordFormDOM = document.querySelector('.edit-form')
+const generalTabDOM = document.querySelector('#account-general')
+const changePasswordTabDOM = document.querySelector('#account-change-password')
+const saveChangesDOM = document.getElementById('save-changes')
+const editProfileFormDOM = document.getElementById('profile-form')
+const changePasswordFormDOM = document.getElementById('edit-form')
+const usernameFieldDOM = document.getElementById('username')
+const nameFieldDOM = document.getElementById('name')
+const phoneFieldDOM = document.getElementById('phone')
 const curPasswordField = document.getElementById('curPassword')
 const newPasswordField = document.getElementById('newPassword')
 const confirmPasswordField = document.getElementById('confirmPassword');
-const formAlertDOM = document.querySelector('.form-alert')
 const params = window.location.search
 const id = new URLSearchParams(params).get('id')
+
+saveChangesDOM.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if (generalTabDOM.classList.contains('active')){
+        editProfileFormDOM.dispatchEvent(new Event('submit'));
+    }
+    if (changePasswordTabDOM.classList.contains('active')){
+        changePasswordFormDOM.dispatchEvent(new Event('submit'));
+    }
+})
+
+editProfileFormDOM.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const username = usernameFieldDOM.value
+    const name = nameFieldDOM.value
+    const phone = phoneFieldDOM.value
+
+    try {
+        const {data} = await axios.patch(`/user/edit-profile`, {name, username, phone})
+        alert(data.msg)
+        window.location.href = '/user'
+    } catch (error) {
+        alert(error.response.data.msg)
+    }
+})
 
 changePasswordFormDOM.addEventListener('submit', async(e)=>{
     e.preventDefault();
@@ -17,8 +48,9 @@ changePasswordFormDOM.addEventListener('submit', async(e)=>{
         curPasswordField.value = '';
         newPasswordField.value = '';
         confirmPasswordField.value = '';
-        formAlertDOM.textContent = data.msg
+        alert(data.msg)
+        window.location.href = '/user'
     } catch (error) {
-        formAlertDOM.textContent = error.response.data.msg
+        alert(error.response.data.msg)
     }
 })
