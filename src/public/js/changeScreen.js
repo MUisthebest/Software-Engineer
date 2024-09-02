0935b2157b86541c90e99b03c5da88d176e9b65c
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2024-09-02 17:24:47 +0700

    fix UI

--- src/public/js/changeScreen.js
+++ src/public/js/changeScreen.js
@@ -1,9 +1,6 @@
-
-
 const toggle = document.querySelector('.toggle');
-
 const point = document.querySelector('section');
-const menu = document.querySelector('.menu')
+const menu = document.querySelector('.menu');
 
 const handleResize = () => {
     const screenWidth = window.innerWidth;
@@ -12,56 +9,80 @@ const handleResize = () => {
     if (screenWidth < 900) {
         nav.classList.add('but');
         toggle.classList.add('but');
-        point.appendChild(menu) 
-        if (menu){
-            menu.onclick = function(){
-                menu.classList.toggle('open')
-            }
-        }   
+        if (!menu) {
+            alert('ok');
+            point.appendChild(menu);
+        }
+        if (menu) {
+            menu.onclick = function() {
+                menu.classList.toggle('open');
+            };
+        }
     } else {
-        point.appendChild(menu) 
-        if (menu){
-            menu.onclick = function(){
-                menu.classList.toggle('open')
-            }
-        }  
-        // if (menu){
-        //     point.removeChild(menu)
-        // }
+        point.appendChild(menu);
+        if (menu) {
+            menu.onclick = function() {
+                menu.classList.toggle('open');
+            };
+        }
         nav.classList.remove('but');
     }
-}
+};
 
 let newX = 0, newY = 0, startX = 0, startY = 0;
 
-const card = document.querySelector('.menu')
+const card = document.querySelector('.menu');
 
 document.addEventListener("DOMContentLoaded", function() {
+    card.addEventListener('mousedown', mouseDown);
+    card.addEventListener('touchstart', touchStart);
 
-card.addEventListener('mousedown', mouseDown)
+    function mouseDown(e) {
+        startX = e.clientX;
+        startY = e.clientY;
 
-function mouseDown(e){
-    startX = e.clientX
-    startY = e.clientY
+        document.addEventListener('mousemove', mouseMove);
+        document.addEventListener('mouseup', mouseUp);
+    }
 
-    document.addEventListener('mousemove', mouseMove)
-    document.addEventListener('mouseup', mouseUp)
-}
+    function touchStart(e) {
+        startX = e.touches[0].clientX;
+        startY = e.touches[0].clientY;
 
-function mouseMove(e){
-    newX = startX - e.clientX 
-    newY = startY - e.clientY 
-  
-    startX = e.clientX
-    startY = e.clientY
+        document.addEventListener('touchmove', touchMove);
+        document.addEventListener('touchend', touchEnd);
+    }
 
-    card.style.top = (card.offsetTop - newY) + 'px'
-    card.style.left = (card.offsetLeft - newX) + 'px'
-}
+    function mouseMove(e) {
+        newX = startX - e.clientX;
+        newY = startY - e.clientY;
 
-function mouseUp(e){
-    document.removeEventListener('mousemove', mouseMove)
-}
+        startX = e.clientX;
+        startY = e.clientY;
+
+        card.style.top = (card.offsetTop - newY) + 'px';
+        card.style.left = (card.offsetLeft - newX) + 'px';
+    }
+
+    function touchMove(e) {
+        newX = startX - e.touches[0].clientX;
+        newY = startY - e.touches[0].clientY;
+
+        startX = e.touches[0].clientX;
+        startY = e.touches[0].clientY;
+
+        card.style.top = (card.offsetTop - newY) + 'px';
+        card.style.left = (card.offsetLeft - newX) + 'px';
+    }
+
+    function mouseUp() {
+        document.removeEventListener('mousemove', mouseMove);
+    }
+
+    function touchEnd() {
+        document.removeEventListener('touchmove', touchMove);
+    }
+
     window.addEventListener('resize', handleResize);
-    handleResize()
+    handleResize();
 });
